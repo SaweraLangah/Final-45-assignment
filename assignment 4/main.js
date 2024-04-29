e66905f640bb47744b3bf50275a1f57ea70c63ad
“[firstname <[valid-email]>
2024-04-28 22:07:40 -0700

    first commit

---
+++ assignment 4/main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Assignment # 4
+let famousQuote = "A person who never made a mistake never tried anything new.";
+let author = "Albert Elistein";
+console.log(`${author} once said ,"${famousQuote}"`);
---
+++ assignment 4/main.ts
@@ -0,0 +1,7 @@
+// Assignment # 4
+
+let famousQuote: string = "A person who never made a mistake never tried anything new." 
+
+let author: string = "Albert Elistein";
+
+console.log(`${author} once said ,"${famousQuote}"`);
